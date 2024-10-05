631cc3d1a3acfbf571f67480799b0b8b85df0c43
Sacha338 <bitton.sacha@gmail.com>
2024-10-05 12:58:32 +0200

    Create the french version

---
+++ French/ScreenTime ServicesTests/ScreenTime_ServicesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScreenTime_ServicesTests.swift
+//  ScreenTime ServicesTests
+//
+//  Created by Tim Schuchardt on 04.10.2024.
+//
+
+import Testing
+
+struct ScreenTime_ServicesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
